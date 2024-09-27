0b529dc623cd78b12865f7acf3c5edcbad860690
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2024-09-27 21:28:31 +0900

    fix: Headerとコンテンツが重なる問題を解決

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-      <header className="bg-gray-100 py-6 text-center fixed top-0 w-full z-20">
+      <header className="bg-gray-100 py-6 text-center top-0 w-full">
         <h1 className="text-2xl font-bold">Hyperionix</h1>
       </header>
     );
