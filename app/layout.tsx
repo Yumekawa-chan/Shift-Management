dcc7dc3ead3de82da780aa84e7be1142b899728a
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2024-10-06 13:42:57 +0900

    update: fontをドットにした

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,18 @@
+'use client';
+
 import './globals.css';
+import localFont from 'next/font/local';
+
+const dotGothic16 = localFont({
+  src: [
+    {
+      path: '../public/DotGothic16-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-dotgothic16', 
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className="overflow-x-hidden bg-gradient-to-br from-pink-50 via-purple-50 to-blue-50 min-h-screen">
+      <body
+        className={`${dotGothic16.className} overflow-x-hidden bg-gradient-to-br from-pink-50 via-purple-50 to-blue-50 min-h-screen`}
+      >
         <div className="relative w-full h-full">
           <div className="absolute top-10 left-10 w-24 h-24 bg-pink-50 rounded-full opacity-40 blur-3xl animate-pulse"></div>
           <div className="absolute bottom-10 right-20 w-32 h-32 bg-blue-50 rounded-full opacity-40 blur-3xl animate-pulse"></div>
